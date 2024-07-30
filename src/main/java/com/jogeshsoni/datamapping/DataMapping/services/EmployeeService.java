2306a8b4aeaccf940b97a5939fb65cfaaf56c6ae
Jogesh Soni <jogeshsoni6@gmail.com>
2024-07-30 23:34:37 +0530

    initial commit

--- src/main/java/com/jogeshsoni/datamapping/DataMapping/services/EmployeeService.java
+++ src/main/java/com/jogeshsoni/datamapping/DataMapping/services/EmployeeService.java
@@ -1,15 +1,19 @@
 package com.jogeshsoni.datamapping.DataMapping.services;
 
+import com.jogeshsoni.datamapping.DataMapping.entities.DepartmentEntity;
 import com.jogeshsoni.datamapping.DataMapping.entities.EmployeeEntity;
+import com.jogeshsoni.datamapping.DataMapping.repositories.DepartmentRepository;
 import com.jogeshsoni.datamapping.DataMapping.repositories.EmployeeRepository;
 import org.springframework.stereotype.Service;
 
 @Service
 public class EmployeeService {
     private final EmployeeRepository employeeRepository;
+    private final DepartmentRepository departmentRepository;
 
-    public EmployeeService(EmployeeRepository employeeRepository) {
+    public EmployeeService(EmployeeRepository employeeRepository, DepartmentRepository departmentRepository) {
         this.employeeRepository = employeeRepository;
+        this.departmentRepository = departmentRepository;
     }
 
     public EmployeeEntity createNewEmployee(EmployeeEntity employeeEntity){
@@ -22,6 +26,14 @@ public class EmployeeService {
 
     public boolean deleteEmployeeById(Long id) {
         if(employeeRepository.existsById(id)){
+            EmployeeEntity employee = getEmployeeById(id);
+
+            for (DepartmentEntity department : employee.getFreelanceDepartments()){
+                department.getFreelancers().remove(employee);
+            }
+
+            departmentRepository.saveAll(employee.getFreelanceDepartments());
+
             employeeRepository.deleteById(id);
             return true;
         }else{
